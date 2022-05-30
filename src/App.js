82eed4697c775a4a3984211fdcaf39dadc64139c
Cesar Torres <torres-cesar@outlook.com>
2022-05-30 16:05:04 -0500

    Style and add navbar elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="text-3x1 font-bold underline">
-      Hello world!
+    <div className='relative flex flex-col 
+      min-h-screen max-w-screen justify-start 
+      bg-gray-600'>
+      <header className='bg-black/60 px-10' >
+        <Navbar />
+      </header>
+      <main className='bg-pink-300 '>main section</main>
+      <footer className='bg-orange-300'>footer section</footer>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav className='relative flex flex-col'>
+      <div className='text-white flex flex-row justify-between'>
+        <a href="https://github.com/cesart98">CT</a>
+        <ul className='gap-2 relative flex flex-row'>
+          <li><a href="https://github.com/cesart98">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/cesart98">LinkedIn</a></li>
+        </ul>
+      </div>
+      <ul className='text-white flex flex-row justify-around gap-12'>
+        <li>about</li>
+        <li>projects</li>
+        <li>contact</li>
+      </ul>
+    </nav>
+  );
+}
